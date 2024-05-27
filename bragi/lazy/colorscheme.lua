ce7c1143c461c252445be31d66c96f051ef41855
bragisivarss <sigursteinnbragi@gmail.com>
2024-05-27 13:24:32 +0000

    kul

--- bragi/lazy/colorscheme.lua
+++ bragi/lazy/colorscheme.lua
@@ -3,9 +3,9 @@ return {
         "morhetz/gruvbox",
         priority = 9999,
         config = function()
-            vim.api.nvim_set_var('gruvbox_contrast_dark', "hard")     -- Contrast
-            vim.api.nvim_set_var('gruvbox_number_column', "dark0_soft")     -- Contrast
-            vim.cmd("colorscheme gruvbox")
+            vim.api.nvim_set_var('gruvbox_contrast_dark', "hard")       -- Contrast
+            vim.api.nvim_set_var('gruvbox_number_column', "dark0_soft") -- Contrast
+            --vim.cmd("colorscheme gruvbox")
         end
     },
     {
@@ -20,7 +20,7 @@ return {
                     light = "latte",
                     dark = "mocha",
                 },
-                transparent_background = false, -- disables setting the background color.
+                transparent_background = true, -- disables setting the background color.
                 show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
                 term_colors = true,             -- sets terminal colors (e.g. `g:terminal_color_0`)
                 dim_inactive = {
@@ -44,45 +44,47 @@ return {
                     properties = {},
                     types = {},
                     operators = {},
-                    -- miscs = {}, -- Uncomment to turn off hard-coded styles
+                    miscs = {}, -- Uncomment to turn off hard-coded styles
                 },
                 color_overrides = {
                     mocha = {
                         -- this 16 colors are changed to onedark
-                        surface0 = "#42133d",
-                        surface1 = "#514b70",
-                        surface2 = "#30274f",
-                        text = "#bbcded",
+                        surface0 = "#621d4b",
+                        surface1 = "#8a3088",
+                        surface2 = "#792c80",
+                        text = "#f6c1f8",
                         rosewater = "#c5ccd9",
-                        lavender = "#c8d0e0",
                         red = "#d9309b",
                         peach = "#e0954f",
                         yellow = "#ebbf6e",
                         green = "#82ba59",
                         teal = "#32bccf",
-                        blue = "#379bed",
+                        blue = "#f8aadb",
                         mauve = "#bf60db",
                         flamingo = "#b83b30",
 
                         -- now patching extra palettes
                         maroon = "#e06c75",
-                        sky = "#d19a66",
+                        --overlay0 = "#c6cce3",
 
                         -- extra colors not decided what to do
                         pink = "#F5C2E7",
                         sapphire = "#74C7EC",
 
-                        subtext1 = "#9fb2f5",
-                        subtext0 = "#8a9ce3",
-                        overlay2 = "#8d9bd6",
-                        overlay1 = "#626da3",
-                        overlay0 = "#424d8a",
+                        subtext1 = "#f9ceff",
+                        subtext0 = "#fb93d4",
+                        overlay1 = "#ac68ae",
+                        overlay0 = "#688e9b",
+                        overlay2 = "#c99d74",
 
-                        mantle = "#19001a",
-                        base = "#0c0017",
-                        crust = "#701f23",
+                        crust = "#c97176",
+                        mantle = "#271d00",
+                        lavender = "#f7b3f9",
+                        base = "#170000",
+
                     },
                 },
+                        --surface1 = "#514b70",
                 custom_highlights = {},
                 default_integrations = true,
                 integrations = {
@@ -124,36 +126,11 @@ return {
             })
 
             -- setup must be called before loading
-            --vim.cmd.colorscheme "catppuccin"
+            vim.cmd.colorscheme "catppuccin"
         end
     },
-    -- {
-    --     "ellisonleao/gruvbox.nvim",
-    --     priority = 1000,
-    --     config = function()
-    --         require("gruvbox").setup({
-    --             palette_overrides = {
-    --                 --  bg0 = "#362c20",
-    --                 --  bg1 = "#262321",
-    --                 --  bg2 = "#403834",
-    --                 --  bg3 = "#42372e",
-    --                 --  bg4 = "#615347",
-    --                 --  fg0 = "#fff7d6",
-    --                 --  fg1 = "#f7e9c3",
-    --                 --  fg2 = "#e0d1b1",
-    --                 --  fg3 = "#ccbda1",
-    --                 --  fg4 = "#f52aa7",
-    --                 --  --fg4 = "#c7b295",
-    --                 --  bright_green = "#b2c73e",
-    --                 --  bright_red = "#ff4567",
-    --                 --  neutral_red = "#ff4567",
-    --                 --  bright_orange = "#e88d2c",
-    --                 --  red = "#960f2f",
-    --                 --  blue = "#58b8ae",
-    --             }
-    --         })
-    --         vim.o.background = "dark" -- or "light" for light mode
-    --         vim.cmd([[colorscheme gruvbox]])
-    --     end
-    -- }
+    {
+        'kepano/flexoki-neovim',
+        name = 'flexoki',
+    }
 }
